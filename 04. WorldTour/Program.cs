ab69d0ab04051c0c4ccfbfd96cba3644f4420d75
Albert Ivanov <albertivanov187@gmail.com>
2025-01-26 01:29:52 +0200

    Update

---
+++ 04. WorldTour/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Numerics;
+using System.Text;
+
+string stops = Console.ReadLine();
+
+string command = Console.ReadLine();
+
+StringBuilder destinations = new StringBuilder(stops);
+
+while (command != "Travel")
+{
+	string[] tokens = command.Split("|");
+
+	if (tokens[0].Contains("Add"))
+	{
+		int index = int.Parse(tokens[1]);
+		string newStop = tokens[2];
+
+		if (isValidIndex(index, stops.Length))
+		{
+
+		}
+
+		destinations.Insert(index, newStop);
+	}
+	else if ()
+	{
+
+	}
+
+	command = Console.ReadLine();
+}
+
+bool isValidIndex(int index, int length)
+{
+	if (index > 0 && index <= length)
+	{
+		return true;
+	}
+	return false;
+}
